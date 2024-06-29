b7f7835689fca75c13ee10ef75088af6e1a2ec03
javiersito <abernalg0128@gmail.com>
2024-06-28 20:41:50 -0500

    delete path calculadora

--- main.py
+++ main.py
@@ -20,9 +20,6 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get ("/calculadora")
-def calcular (operando_1 : float, operando_2: float):
-    return {'suma': operando_1 + operando_2}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
