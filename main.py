04343ffb0df36cbe1f3909f186f7de3447d8ad34
javiersito <abernalg0128@gmail.com>
2024-06-28 20:32:51 -0500

    new operation

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 def read_root():
@@ -14,8 +20,11 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get ("/calculadora") # Metodo para crear un parametro de ruta de tipo GET
-# Creacion de la funcion la cual servira en este path
+@app.get ("/calculadora")
 def calcular (operando_1 : float, operando_2: float):
     return {'suma': operando_1 + operando_2}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
 
